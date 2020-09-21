a3303b1466a56e1b376e6b912a6d7c85e7a240e9
nizutaro <niduka81@gmail.com>
2020-09-22 01:56:34 +0900

    [add]DMM_3章追加

--- javascript_practice.js
+++ javascript_practice.js
@@ -1,3 +1,5 @@
+//2章
+
 var hello = "hello World";
 alert(hello);
 
@@ -41,3 +43,87 @@ for (i = 1; i < 11; i++) {
 }
 
 alert("1から10までの足し算した結果は" + num + "です");
+
+//3章
+
+//メイン部分
+var alertString;
+alertString = addString("webCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+//作成した関数
+function addString(strA) {
+  var addStr = "Hello " + strA;
+  return addStr;
+}
+
+//Math.floor() メソッドは、引数として与えた数以下の最大の整数を返します。
+//Math は、数学的な定数と関数を提供するプロパティとメソッドを持つ、組み込みのオブジェクトです。関数オブジェクトではありません。
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+var user_hand = prompt(
+  "じゃんけんの手をグー、チョキ、パーから選んでください。"
+);
+
+// じゃんけんの手をランダムに作成する関数を呼び出す
+var js_hand = getJShand();
+
+//ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+var judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+alert(
+  "あなたの選んだ手は" +
+    user_hand +
+    "です。\nJavascriptの選んだ手は" +
+    js_hand +
+    "です。\n結果は" +
+    judge +
+    "です。"
+);
+//ランダムでじゃんけんの手を作成する関数
+function getJShand() {
+  var js_hand_num = Math.floor(Math.random() * 3);
+  var hand_name;
+  if (js_hand_num == 0) {
+    hand_name = "グー";
+  } else if (js_hand_num == 1) {
+    hand_name = "チョキ";
+  } else if (js_hand_num == 2) {
+    hand_name = "パー";
+  }
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js) {
+  var winLoseStr;
+
+  if (user == "グー") {
+    if (js == "グー") {
+      winLoseStr = "あいこ";
+    } else if (js == "チョキ") {
+      winLoseStr = "勝ち";
+    } else if (js == "パー") {
+      winLoseStr = "負け";
+    }
+  } else if (user == "チョキ") {
+    if (js == "グー") {
+      winLoseStr = "負け";
+    } else if (js == "チョキ") {
+      winLoseStr = "あいこ";
+    }
+  } else if (user == "パー") {
+    if (js == "グー") {
+      winLoseStr = "勝ち";
+    } else if (js == "チョキ") {
+      winLoseStr = "負け";
+    } else if (js == "パー") {
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
